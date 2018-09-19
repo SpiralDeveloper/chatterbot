f8516cf51fe1da166eb0fefa02a783f870df1ff9
unknown <ashish@bitsbi.com>
2018-09-20 04:43:34 +0530

    init

---
+++ app/src/main/java/chatterbot/app/bits/fabric/chatterbot/MainActivity.java
@@ -0,0 +1,23 @@
+package chatterbot.app.bits.fabric.chatterbot;
+
+import android.support.v4.view.ViewCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+
+import fabric.bits.api.rdk.RdkManager;
+import fabric.bits.api.rdk.annotations.RLayout;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ chatterbot/src/main/java/bits/fabric/chatterbot/Chatterbot.java
@@ -0,0 +1,6 @@
+package bits.fabric.chatterbot;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class Chatterbot extends AppCompatActivity{
+}
